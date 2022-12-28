496598b51b4eedaed6d64e8aff13717f1336c651
camelCase0 <camelCase0>
2022-12-28 18:31:08 +0200

    Kalvados

--- app/models.py
+++ app/models.py
@@ -26,7 +26,7 @@ class Status(Enum):
 
 class User(Base):
     __tablename__ = 'users'
-
+    
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String)
     password = Column(String, nullable=False)
