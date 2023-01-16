96bc0c3fc46fc86717e51c0ed27c366ecaac1c02
camelCase0 <pashok2082.ryuz@gmail.com>
2023-01-16 21:05:01 +0200

    fixed docker JWT.encode error

--- app/config.py
+++ app/config.py
@@ -8,13 +8,16 @@ root_dir = dir_path[:-3]
 
 config = Config(f'{root_dir}.env')
 
-# DATABASE_URL = f'sqlite:///{root_dir}' + config('DB_NAME', cast=str)
+# DATABASE_URL = f'sqlite:///{root_dir}' + config('POSTGRES_HOST', cast=str) + '.db'
+
 user = config('POSTGRES_USER', cast=str)
 password = config('POSTGRES_PASSWORD', cast=str)
 host = config('POSTGRES_HOST', cast=str)
 db_name = config('POSTGRES_DB', cast=str)
 
 DATABASE_URL = f'postgresql://{user}:{password}@{host}:5432/{db_name}'
+
 # '+config('POSTGRES_PORT', cast=str) + '
+
 SECRET_KEY = config("SECRET_KEY",cast=str)
 ALGORITHM = config("ALGORITHM",cast=str)
