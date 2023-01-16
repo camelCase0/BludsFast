8676a5a4f279d4f0795da8a0145caca66ef2dcce
camelCase0 <pashok2082.ryuz@gmail.com>
2023-01-16 17:46:08 +0200

    added Postgresql docker compose

--- app/database.py
+++ app/database.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm import sessionmaker
 from app.config import DATABASE_URL
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    # DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
